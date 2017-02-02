d6369bdcb3b7f626fb378c6bc50265eafd54a9f4
Andi DeVois <adevois@gmail.com>
2017-02-01 20:01:05 -0600

    regex written with names for each group

---
+++ SimpleCalculator.Tests/ExpressionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        //sanity check test
+        [TestMethod]
+        public void CanCreateAnInstance()
+        {
+            Expression expression = new Expression();
+            Assert.IsNotNull(expression);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
---
+++ SimpleCalculator/Expression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Expression
+    {
+        string regExEval = @"\b(?<FirstNum>\d+)\s*\b(?<Operator>[\+\/\-\+\%])\s*\b(?<SecondNum>\d+)";
+
+    }
+}
