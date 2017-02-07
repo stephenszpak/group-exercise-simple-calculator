60256de7a103c88001e8f1f9513acff37258381b
Andi DeVois <adevois@gmail.com>
2017-02-06 20:18:36 -0600

    expression test starting point

--- SimpleCalculator.Tests/ExpressionTests.cs
+++ SimpleCalculator.Tests/ExpressionTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
 
 namespace SimpleCalculator.Tests
 {
@@ -15,8 +16,18 @@ namespace SimpleCalculator.Tests
         }
 
         [TestMethod]
-        public void MyTestMethod()
+        public void CanCheckInput()
         {
+            Expression testExpression = new Expression();
+            string testPattern = @"\b(?<FirstNum>\d+)\s*\b(?<Operator>[\+\/\-\+\%])\s*\b(?<SecondNum>\d+)";
+            Regex Regex = new Regex(testPattern);
+
+            string test1 = "1+1";
+            string test2 = "2 + 2";
+
+
+            Assert.IsTrue(Regex.IsMatch(test1));
+            Assert.IsTrue(Regex.IsMatch(test2));
 
         }
     }
--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
     public class Expression
     {
-        string regExEval = @"\b(?<FirstNum>\d+)\s*\b(?<Operator>[\+\/\-\+\%])\s*\b(?<SecondNum>\d+)";
+        string testPattern = @"\b(?<FirstNum>\d+)\s*\b(?<Operator>[\+\/\-\+\%])\s*\b(?<SecondNum>\d+)";
+
+        public void StringSplitter (string UserInput)
+        {
+            Regex Regex = new Regex(testPattern);
+
+
+            if (true == Regex.IsMatch(UserInput))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("input is invalid");
+            }
+        }
 
     }
 }
